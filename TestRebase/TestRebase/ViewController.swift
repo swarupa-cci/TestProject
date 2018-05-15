5a045ffcea6392241ddf3c328e997f6cd516caee
Swarupa Vengurlekar <swarupa@creativecapsule.com>
2018-05-15 16:34:38 +0530

    2

--- TestRebase/TestRebase/ViewController.swift
+++ TestRebase/TestRebase/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
        var x = "Hi"
+       var y = "2"
     }
 
     override func didReceiveMemoryWarning() {
