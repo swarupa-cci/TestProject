4e19dfd70ae51afa6d6b6a4883a35921a5af73f5
Swarupa Vengurlekar <swarupa@creativecapsule.com>
2018-05-15 16:35:37 +0530

    3

--- TestRebase/TestRebase/ViewController.swift
+++ TestRebase/TestRebase/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var t = "3"
     }
 
     override func didReceiveMemoryWarning() {
